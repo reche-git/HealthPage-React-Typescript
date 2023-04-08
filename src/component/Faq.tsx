06a1d7d07b4cffd51b654f8f3c04003471d2b4d7
Alan Reche <alanaxelreche@hotmail.com>
2023-04-08 17:47:41 -0300

    typos + styles

--- src/component/Faq.tsx
+++ src/component/Faq.tsx
@@ -6,15 +6,35 @@ const Faq = () => {
         <div className="accordion-flush faq" id="accordionFlushExample">
           <div className="section-title">
             <h2>Frequently Asked Questions</h2>
-            <p>
-              Magnam dolores commodi suscipit. Necessitatibus eius consequatur
-              ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
-              quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-              Quia fugiat sit in iste officiis commodi quidem hic quas.
-            </p>
           </div>
           <div className="faq-list">
             <ul>
+              <li className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingFour">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseFour"
+                    aria-expanded="false"
+                    aria-controls="flush-collapseFour"
+                  >
+                    <i className="bi bi-patch-question icon-help"></i>
+                    How can I affiliate to PokeCare?
+                  </button>
+                </h2>
+                <div
+                  id="flush-collapseFour"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingFour"
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <div className="accordion-body">
+                    You just need to go to one of our clinics and ask to get
+                    affiliated. Remember to bring your ID.
+                  </div>
+                </div>
+              </li>
               <li className="accordion-item">
                 <h2 className="accordion-header" id="flush-headingOne">
                   <button
@@ -26,7 +46,7 @@ const Faq = () => {
                     aria-controls="flush-collapseOne"
                   >
                     <i className="bi bi-patch-question icon-help"></i>
-                    Accordion Item #1
+                    Is there a fee for using the PokeCare clinics?
                   </button>
                 </h2>
                 <div
@@ -36,9 +56,11 @@ const Faq = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> className.
-                    This is the first item's accordion body.
+                    The urgent care is free for all Pokémon! However, emergency
+                    care fees can vary depending of your country Public Pokémon
+                    Health Plan. Residents from one country will face the same
+                    fees around the world since their plans are tied to their
+                    referred countries health plan.
                   </div>
                 </div>
               </li>
@@ -53,7 +75,9 @@ const Faq = () => {
                     aria-controls="flush-collapseTwo"
                   >
                     <i className="bi bi-patch-question icon-help"></i>
-                    Accordion Item #2
+                    I'm a tourist/trainer that flew to another country/continent
+                    and my Pokémon got hurt. Can I take him to a PokeCare
+                    clinic?
                   </button>
                 </h2>
                 <div
@@ -63,10 +87,9 @@ const Faq = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> className.
-                    This is the second item's accordion body. Let's imagine this
-                    being filled with some actual content.
+                    Of course! Even if you are not affiliated with us, we' wi'll
+                    take care of your Pokémon for you. Please notice that you
+                    will be charged for the treatment afterwards.
                   </div>
                 </div>
               </li>
@@ -81,7 +104,8 @@ const Faq = () => {
                     aria-controls="flush-collapseThree"
                   >
                     <i className="bi bi-patch-question icon-help"></i>
-                    Accordion Item #3
+                    I'm an student and I'll like to do my residency with you.
+                    Can this be done?
                   </button>
                 </h2>
                 <div
@@ -91,13 +115,9 @@ const Faq = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> className.
-                    This is the third item's accordion body. Nothing more
-                    exciting happening here in terms of content, but just
-                    filling up the space to make it look, at least at first
-                    glance, a bit more representative of how this would look in
-                    a real-world application.
+                    Check with your school, you can probably find us in your
+                    affiliated medical center list! If not, we have private
+                    residency plans with payment plans and scholarship plans!
                   </div>
                 </div>
               </li>
